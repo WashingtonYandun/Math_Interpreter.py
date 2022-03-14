7e784f94fe3ba611819f69d9d8ac8a4960c5f1f1
WashingtonYandun <washington.yandun06@gmail.com>
2022-03-14 15:21:05 -0500

    separate classes in other files

--- Token.py
+++ Token.py
@@ -1,27 +1,5 @@
 from dataclasses import dataclass
-from enum import Enum
-
-
-iota_count = 0
-
-
-def iota(restart=False):
-    global iota_count
-    if restart:
-        iota_count = 0
-    c = iota_count
-    iota_count = iota_count + 1
-    return c
-
-
-class TokenType(Enum):
-    NUMBER = iota()
-    PLUS = iota()
-    MINUS = iota()
-    PRODUCT = iota()
-    DIVIDE = iota()
-    L_PAR = iota()
-    R_PAR = iota()
+from TokenType import TokenType
 
 
 @dataclass
